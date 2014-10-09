45b1c100c252762422568a3b44c8e19d358d935f
frbl <frank.blaauw@gmail.com>
2014-10-09 17:09:32 +0200

    Added previous and next events

--- assets/js/controllers/about_controller.js
+++ assets/js/controllers/about_controller.js
@@ -1,10 +1,18 @@
 frblControllers.controller('AboutCtrl', ['$scope', 'GoogleCalendarService',
   function ($scope, GoogleCalendarService) {
     $scope.current_location = 'Loading location..'
-    GoogleCalendarService.refresh().then(function(payload) {
-      angular.forEach(payload.data.items, function(data) {
-        $scope.current_location = data.summary;
+    $scope.date = new Date();
+    
+    $scope.location_of_day = function(offset) {
+      $scope.date.setDate($scope.date.getDate() + offset);
+      GoogleCalendarService.refresh($scope.date).then(function(payload) {
+        $scope.current_location = 'N/A'
+        angular.forEach(payload.data.items, function(data) {
+          $scope.current_location = data.summary;
+        });
       });
-    });
+    }
+    
+    $scope.location_of_day(0);
   }
 ]);
--- assets/js/services/google_calendar_service.js
+++ assets/js/services/google_calendar_service.js
@@ -3,25 +3,24 @@ frblServices.factory('GoogleCalendarService', function($http) {
   var key = 'AIzaSyBMmyQ4f4JdJBfmbAfwsy0Ag7giNh4tbM0';
   var calendar_id = '4jsjonjdekan0b8ndch0o12ja8@group.calendar.google.com';
   
-  function build_url(id, key) {
-    return apppend_day(append_key('https://www.googleapis.com/calendar/v3/calendars/' + id + '/events', key));
+  function build_url(id, key, date) {
+    return apppend_date_range(append_key('https://www.googleapis.com/calendar/v3/calendars/' + id + '/events', key), date);
   }
   
   function append_key(url, key) {
     return (url + '?key=' + key);
   }
   
-  function apppend_day(url) {
-    start = new Date();
-    start.setHours(0,0,0,0);
-    end = new Date();
-    end.setHours(23,59,59,999);
-    return url + '&timeMin=' + start.toISOString()+'&timeMax='+end.toISOString();
+  function apppend_date_range(url, date) {
+    var end = angular.copy(date);
+    date.setUTCHours(0,0,0,0);
+    end.setUTCHours(23,59,59,999);
+    return url + '&timeMin=' + date.toISOString()+'&timeMax='+end.toISOString();
   }
     
   return {
-    refresh: function() {
-      return $http.get(build_url(calendar_id, key));
+    refresh: function(date) {
+      return $http.get(build_url(calendar_id, key, date));
     }
   };  
 });
