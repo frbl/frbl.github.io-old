b53b3cf8158b9ba395bdde2f967a5024b939a1aa
Frank Blaauw <frank.blaauw@gmail.com>
2017-11-30 15:46:59 +0100

    Updated address

--- assets/js/controllers/contact_controller.js
+++ assets/js/controllers/contact_controller.js
@@ -3,18 +3,17 @@ frblControllers.controller('ContactCtrl', ['$scope', '$document',
         $scope.options = {scrollwheel:false, streetViewControl:false, mapTypeControl:false, panControl:false};
         $scope.map = { center: { latitude: 53.240670, longitude: 6.536378 }, zoom: 17 };
         $scope.rug = true;
-        $scope.umcg = false;
+        $scope.heymans = false;
         $scope.set_loc = function(name) {
             if(name == 'rug') {
                 $scope.rug = true;
-                $scope.umcg = false;
+                $scope.heymans = false;
                 $scope.map = { center: { latitude: 53.240670, longitude: 6.536378 }, zoom: 17 };
             }
-            else if(name == 'umcg'){
+            else if(name == 'heymans'){
                 $scope.rug = false;
-                $scope.umcg = true;
-
-                $scope.map = { center: { latitude: 53.224018, longitude: 6.575416 }, zoom: 17 };
+                $scope.heymans = true;
+                $scope.map = { center: { latitude: 53.2230132, longitude: 6.5574806 }, zoom: 17 };
 
             }
         };
